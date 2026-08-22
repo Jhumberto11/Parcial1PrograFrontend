03785b16c4cb86abfb49a0eeb8c39037875cce2b
CarlosR <ElMar@XCARLOS7>
2026-08-21 22:48:46 -0600

    Implementación de CRUD (Edit y Delete) para Productos y actualización de tarjeta parcial

--- TechStore/Controllers/ProductosController.cs
+++ TechStore/Controllers/ProductosController.cs
@@ -35,5 +35,74 @@ namespace TechStore.Controllers
 
             return View(producto);
         }
-    }
+        // =============================
+        // SECCIÓN: EDITAR PRODUCTO
+        // =============================
+
+        public IActionResult Edit(int id)
+        {
+            var producto = Datos.Productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+           
+            ViewBag.Categorias = Datos.Categorias;
+            return View(producto);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Producto productoModificado)
+        {
+            var productoOriginal = Datos.Productos.FirstOrDefault(p => p.Id == productoModificado.Id);
+
+            if (productoOriginal != null)
+            {
+                
+                productoOriginal.Nombre = productoModificado.Nombre;
+                productoOriginal.Descripcion = productoModificado.Descripcion;
+                productoOriginal.Precio = productoModificado.Precio;
+                productoOriginal.Stock = productoModificado.Stock;
+                productoOriginal.Imagen = productoModificado.Imagen;
+                productoOriginal.CategoriaId = productoModificado.CategoriaId;
+                productoOriginal.Destacado = productoModificado.Destacado;
+
+                return RedirectToAction("Index");
+            }
+
+         
+            ViewBag.Categorias = Datos.Categorias;
+            return View(productoModificado);
+        }
+
+        // =============================
+        // SECCIÓN: ELIMINAR PRODUCTO
+        // =============================
+
+    
+        public IActionResult Delete(int id)
+        {
+            var producto = Datos.Productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var producto = Datos.Productos.FirstOrDefault(p => p.Id == id);
+
+            if (producto != null)
+            {
+                Datos.Productos.Remove(producto);
+            }
+
+            return RedirectToAction("Index");
+        }
+    } 
 }
